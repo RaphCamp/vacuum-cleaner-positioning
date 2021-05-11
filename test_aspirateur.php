5883cc086f7622cd1d17711f42a1bb27955ea620
Raphael <raphael.campagnac@live.fr>
2021-05-11 20:17:15 +0200

    correction commentaires

--- test_aspirateur.php
+++ test_aspirateur.php
@@ -4,11 +4,11 @@ require "Position.php";
 
 
 
-//On définit les paramètre d'entrée de notre aspirateur (dimention de la grille en x et y , position initiale en x et y, orientation intitale)
+//On définit les paramètres d'entrée de notre aspirateur (dimension de la grille en x et y , position initiale en x et y, orientation initiale)
 $aspirateur1 = new Position(10,10,5,5,'N'); 
 
 
-//On appelle la méthode "bouger" en rentrant le parametre de mouvement (A -> anvancer ; D -> trouner à droite ; G -> tourner à gauche)
+//On appelle la méthode "bouger" en rentrant le paramètre de mouvement (A -> avancer ; D -> tourner à droite ; G -> tourner à gauche)
 $aspirateur1->bouger('D');
 $aspirateur1->bouger('A');
 $aspirateur1->bouger('D');
@@ -17,7 +17,7 @@ $aspirateur1->bouger('D');
 $aspirateur1->bouger('A');
 $aspirateur1->bouger('D');
 $aspirateur1->bouger('A');
-$aspirateur1->bouger('A');
+$aspirateur1->bouger('G');
 
 //On appelle la méthode "voirPosition" pour vérifier la nouvelle position de l'aspirateur
 $aspirateur1->voirPosition();
